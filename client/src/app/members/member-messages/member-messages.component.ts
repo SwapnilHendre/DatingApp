b5cc9de2e61ebff7f36cc4eca813143fe5df1416
SwapnilHendre <swapnilhendre1@gmail.com>
2023-06-26 18:05:00 +0530

    End of Section 17

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -12,18 +12,14 @@ export class MemberMessagesComponent {
 
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor(private messageService: MessageService) {}
+  constructor(public messageService: MessageService) {}
 
   sendMessage() {
     if(!this.username) return;
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
-      next: message => {
-        this.messages.push(message);
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
         this.messageForm?.reset();
-      }
-    });
+      });
   }
 }
